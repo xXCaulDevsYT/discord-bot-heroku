2822c8544a74ec0eb1b095fdfb31a1e8ecd54855
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-05-02 18:02:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,26 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '+help') {
-    	message.reply('iGalactic `BOT` Was Made / Developed By `xXCaulDevsYT#7314` There Are Currently **__1__** Commands: `+help (shows a list of commands`');
+    if (message.content === '+bothelp') {
+    	message.reply(' Commands: `+help (shows a list of commands` , `+donate (donate to the bot)` , `+botinfo (show info about the bot)`, `+botyoutube (shows yt)`!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '+botinfo') {
+    	message.reply('**__iGalactic Bot__** » **Developed By** `@xXCaulDevsYT#7314` «');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '+donate') {
+    	message.reply('**__Donate Here__** » https://paypal.me/caulden | Donations Help Keep The Bot Up! «');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '+botyoutube') {
+    	message.reply('**YOUTUBE:** » https://youtube.com/emeraldassasinplayz «');
   	}
 });
 
